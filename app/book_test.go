17fd371c7ab3323563af0edbafe3731e73ffc1d4
Victor Stein <victor.stein@outlook.com>
2022-06-29 20:48:27 -0300

    test: Adicionando teste da função enable

---
+++ app/book_test.go
@@ -0,0 +1,22 @@
+package app_test
+
+import (
+	"ports-and-adapter-with-go/app"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBook_Enable(t *testing.T) {
+	book := app.Book{}
+	book.Name = "Revolução dos Bichos"
+	book.Status = app.DISABLED
+	book.Price = 29.99
+
+	err := book.Enable()
+	require.Nil(t, err)
+
+	book.Price = 0
+	err = book.Enable()
+	require.Equal(t, "O preço do livro deve ser maior que zero para habilitar o livro!", err.Error())
+}
